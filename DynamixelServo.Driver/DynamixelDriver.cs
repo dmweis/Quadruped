7c752ff179bee6334ee9da917550f2d03e12cf9b
David Weis <davidmichaelweis@gmail.com>
2017-06-10 19:16:04 +0200

    Initial commit

---
+++ DynamixelServo.Driver/DynamixelDriver.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using dynamixel_sdk;
+
+namespace DynamixelServo.Driver
+{
+   class DynamixelDriver : IDisposable
+   {
+      // Control table address
+      public const ushort ADDR_TORQUE_ENABLE = 24;
+      public const ushort ADDR_LED_ENABLE = 25;
+      public const ushort ADDR_GOAL_POSITION = 30;
+      public const ushort ADDR_MOVING_SPEED = 32;
+      public const ushort ADDR_PRESENT_POSITION = 36;
+      public const ushort ADDR_PRESENT_SPEED = 38;
+      public const ushort ADDR_PRESENT_LOAD = 40;
+      public const ushort ADDR_PRESENT_VOLTAGE = 42;
+      public const ushort ADDR_PRESENT_TEMP = 43;
+
+      private const int ProtocolVersion = 1;
+      private const int BaudRate = 1000000;
+      private const int CommFail = -1001;
+      private const int CommSuccess = 0;
+
+      private readonly string _portName;
+      private readonly int _portNumber;
+
+      public DynamixelDriver(string portName)
+      {
+         _portName = portName;
+         _portNumber = dynamixel.portHandler(_portName);
+         dynamixel.packetHandler();
+
+
+         if (!dynamixel.openPort(_portNumber))
+         {
+            throw new IOException($"Can not open port {_portName}");
+         }
+         if (!dynamixel.setBaudRate(_portNumber, BaudRate))
+         {
+            throw new IOException($"Can not set baud rate {BaudRate} on: {_portName}");
+         }
+      }
+
+      public byte[] Search(byte startId = 0, byte endId = 252)
+      {
+         List<byte> found = new List<byte>();
+         for (byte i = startId; i < endId; i++)
+         {
+            try
+            {
+               Ping(i);
+               found.Add(i);
+            }
+            catch (IOException)
+            {
+
+            }
+         }
+         return found.ToArray();
+      }
+
+      public void SetLed(byte servoId, bool on)
+      {
+         byte ledFlag = on ? (byte)1 : (byte)0;
+         WriteByte(servoId, ADDR_LED_ENABLE, ledFlag);
+      }
+
+      public void SetTorque(byte servoId, bool on)
+      {
+         byte torqueFlag = on ? (byte)1 : (byte)0;
+         WriteByte(servoId, ADDR_TORQUE_ENABLE, torqueFlag);
+      }
+
+      public void WriteGoalPosition(byte servoId, ushort goalPosition)
+      {
+         WriteUInt16(servoId, ADDR_GOAL_POSITION, goalPosition);
+      }
+
+      public void WriteMovingSpeed(byte servoId, ushort movingSpeed)
+      {
+         WriteUInt16(servoId, ADDR_MOVING_SPEED, movingSpeed);
+      }
+
+      public ushort ReadPresentPosition(byte servoId)
+      {
+         return ReadUInt16(servoId, ADDR_PRESENT_POSITION);
+      }
+
+      public ushort ReadPresentSpeed(byte servoId)
+      {
+         return ReadUInt16(servoId, ADDR_PRESENT_SPEED);
+      }
+
+      public ushort ReadPresentLoad(byte servoId)
+      {
+         return ReadUInt16(servoId, ADDR_PRESENT_LOAD);
+      }
+
+      public byte ReadTemperature(byte servoId)
+      {
+         return ReadByte(servoId, ADDR_PRESENT_TEMP);
+      }
+
+      public byte ReadVoltage(byte servoId)
+      {
+         return ReadByte(servoId, ADDR_PRESENT_VOLTAGE);
+      }
+
+      public ushort GetModelNumber(byte servoId)
+      {
+         ushort modelNumber = dynamixel.pingGetModelNum(_portNumber, ProtocolVersion, servoId);
+         VerifyLastMessage();
+         return modelNumber;
+      }
+
+      public void Ping(byte servoId)
+      {
+         dynamixel.ping(_portNumber, ProtocolVersion, servoId);
+         VerifyLastMessage();
+      }
+
+      private void WriteByte(byte servoId, ushort address, byte data)
+      {
+         dynamixel.write1ByteTxRx(_portNumber, ProtocolVersion, servoId, address, data);
+         VerifyLastMessage();
+      }
+
+      private byte ReadByte(byte servoId, ushort address)
+      {
+         byte incoming = dynamixel.read1ByteTxRx(_portNumber, ProtocolVersion, servoId, address);
+         VerifyLastMessage();
+         return incoming;
+      }
+
+      private void WriteUInt16(byte servoId, ushort address, ushort data)
+      {
+         dynamixel.write2ByteTxRx(_portNumber, ProtocolVersion, servoId, address, data);
+         VerifyLastMessage();
+      }
+
+      private ushort ReadUInt16(byte servoId, ushort address)
+      {
+         ushort incoming = dynamixel.read2ByteTxRx(_portNumber, ProtocolVersion, servoId, address);
+         VerifyLastMessage();
+         return incoming;
+      }
+
+      private void VerifyLastMessage()
+      {
+         byte dxlError = 0;
+         int commResult = CommFail;
+         if ((commResult = dynamixel.getLastTxRxResult(_portNumber, ProtocolVersion)) != CommSuccess)
+         {
+            throw new IOException(DynamixelErrorHelper.GetTxRxResultDescription(commResult));
+         }
+         if ((dxlError = dynamixel.getLastRxPacketError(_portNumber, ProtocolVersion)) != 0)
+         {
+            throw new IOException(DynamixelErrorHelper.GetRxPackErrorDescription(dxlError));
+         }
+      }
+
+      public void Dispose()
+      {
+         dynamixel.closePort(_portNumber);
+      }
+   }
+}
