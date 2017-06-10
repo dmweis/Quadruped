35e3988b198e6dc7c6aaaafa560fe4e8be52367a
David Weis <davidmichaelweis@gmail.com>
2017-06-11 01:06:25 +0200

    Add simple monitor to WPF

--- DynamixelServo.WPFMonitor/App.xaml.cs
+++ DynamixelServo.WPFMonitor/App.xaml.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using GalaSoft.MvvmLight.Threading;
 
 namespace DynamixelServo.WPFMonitor
 {
@@ -13,5 +8,9 @@ namespace DynamixelServo.WPFMonitor
    /// </summary>
    public partial class App : Application
    {
+      static App()
+      {
+         DispatcherHelper.Initialize();
+      }
    }
 }
--- DynamixelServo.WPFMonitor/MainWindow.xaml.cs
+++ DynamixelServo.WPFMonitor/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Timers;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using DynamixelServo.Driver;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using GalaSoft.MvvmLight.Threading;
 
 namespace DynamixelServo.WPFMonitor
 {
@@ -20,9 +14,79 @@ namespace DynamixelServo.WPFMonitor
    /// </summary>
    public partial class MainWindow : Window
    {
+      public ObservableCollection<Servo> Servos { get; } = new ObservableCollection<Servo>();
+      private DynamixelDriver _driver;
+
       public MainWindow()
       {
          InitializeComponent();
+         DataContext = this;
+         _driver = new DynamixelDriver("COM17");
+         foreach (var index in _driver.Search(1, 10))
+         {
+            Servo newServo = new Servo() {Index = index};
+            newServo.ToggleLed += (sender, servo) =>
+            {
+               _driver.SetLed((byte)servo.Index, servo.LedOn);
+            };
+            Servos.Add(newServo);
+         }
+         foreach (var servo in Servos)
+         {
+            _driver.SetTorque((byte)servo.Index, false);
+         }
+         Timer timer = new Timer();
+         timer.AutoReset = true;
+         timer.Elapsed += (sender, args) =>
+         {
+            DispatcherHelper.CheckBeginInvokeOnUI(() =>
+            {
+               foreach (var servo in Servos)
+               {
+                  servo.Temperature = _driver.ReadTemperature((byte) servo.Index);
+                  servo.Position = _driver.ReadPresentPosition((byte) servo.Index);
+               }
+            });
+         };
+         timer.Interval = 100;
+         timer.Start();
+      }
+   }
+
+   public class Servo : ViewModelBase
+   {
+      public event EventHandler<Servo> ToggleLed;
+      public bool LedOn;
+      public RelayCommand ToggleLedCommand { get; }
+
+      private int _temperature;
+      public int Temperature
+      {
+         get => _temperature;
+         set => Set(ref _temperature, value);
+      }
+
+      private int _index;
+      public int Index
+      {
+         get => _index;
+         set => Set( ref _index, value);
+      }
+
+      private int _position;
+      public int Position
+      {
+         get => _position;
+         set => Set( ref _position , value);
+      }
+
+      public Servo()
+      {
+         ToggleLedCommand = new RelayCommand(() =>
+         {
+            LedOn = !LedOn;
+            ToggleLed?.Invoke(this, this);
+         });
       }
    }
 }
