8fed757d71d3e70a34d7da1104c10e752da738d5
David Weis <davidmichaelweis@gmail.com>
2017-06-13 11:07:45 +0200

    Add continuouse recording method

--- DynamixelServo.TestConsole/Program.cs
+++ DynamixelServo.TestConsole/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading;
 using DynamixelServo.Driver;
+using Timer = System.Timers.Timer;
 
 namespace DynamixelServo.TestConsole
 {
@@ -9,16 +10,68 @@ namespace DynamixelServo.TestConsole
    {
       static void Main(string[] args)
       {
-         //Record();
+         Record();
+         Console.WriteLine("Starting");
+         //using (DynamixelDriver driver = new DynamixelDriver("COM17"))
+         //{
+         //   driver.WriteMovingSpeed(1, 0, DynamixelProtocol.Version2);
+         //   driver.WriteGoalPosition(1, 0, DynamixelProtocol.Version2);
+         //   Thread.Sleep(1000);
+         //   driver.WriteGoalPosition(1, 1023, DynamixelProtocol.Version2);
+         //   Thread.Sleep(1000);
+         //   driver.WriteGoalPosition(1, 0, DynamixelProtocol.Version2);
+         //}
+         //Console.WriteLine("Press enter to exit");
+         //Console.ReadLine();
+      }
+
+      public static void RecordContinuouse()
+      {
          Console.WriteLine("Starting");
          using (DynamixelDriver driver = new DynamixelDriver("COM17"))
          {
-            driver.WriteMovingSpeed(1, 0, DynamixelProtocol.Version2);
-            driver.WriteGoalPosition(1, 0, DynamixelProtocol.Version2);
-            Thread.Sleep(1000);
-            driver.WriteGoalPosition(1, 1023, DynamixelProtocol.Version2);
-            Thread.Sleep(1000);
-            driver.WriteGoalPosition(1, 0, DynamixelProtocol.Version2);
+            byte[] servoIds = driver.Search(1, 10);
+            IList<ushort[]> history = new List<ushort[]>();
+            foreach (var id in servoIds)
+            {
+               driver.WriteMovingSpeed(id, 100);
+               driver.SetTorque(id, false);
+            }
+            Console.WriteLine("press enter to start recording");
+            Console.ReadLine();
+            using (Timer timer = new Timer())
+            {
+               timer.Interval = 100;
+               timer.AutoReset = true;
+               timer.Elapsed += (sender, args) =>
+               {
+                  ushort[] currentPositions = new ushort[servoIds.Length];
+                  for (int i = 0; i < servoIds.Length; i++)
+                  {
+                     currentPositions[i] = driver.ReadPresentPosition(servoIds[i]);
+                  }
+                  history.Add(currentPositions);
+               };
+               Console.WriteLine("Started recording. Press enter to stop!");
+               timer.Start();
+               Console.ReadLine();
+               timer.AutoReset = false;
+               Thread.Sleep(200);
+            }
+            while (true)
+            {
+               foreach (var positions in history)
+               {
+                  DynamixelHelpers.MoveToAll(servoIds, positions, driver);
+                  Thread.Sleep(100);
+               }
+               Console.WriteLine("write q to exit");
+               string input = Console.ReadLine();
+               if (input == "q")
+               {
+                  break;
+               }
+            }
          }
          Console.WriteLine("Press enter to exit");
          Console.ReadLine();
