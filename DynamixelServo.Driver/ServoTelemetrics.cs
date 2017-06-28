9de32235b7747336ec0c61c64a0919f6a4ffd7c8
David Weis <davidmichaelweis@gmail.com>
2017-06-28 23:14:06 +0200

    Add servo telemetrics object to ddriver project

---
+++ DynamixelServo.Driver/ServoTelemetrics.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamixelServo.Driver
+{
+   class ServoTelemetrics
+   {
+      public int Id { get; set; }
+      public int Temperature { get; set; }
+      public float Voltage { get; set; }
+      public int Load { get; set; }
+
+   }
+}
