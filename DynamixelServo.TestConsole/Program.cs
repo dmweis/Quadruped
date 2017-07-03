0349fe20a14c6576ba7dac6384ce348e6668b9ec
David Weis <davidmichaelweis@gmail.com>
2017-07-04 00:37:56 +0200

    Add servo position brodcaster

--- DynamixelServo.TestConsole/Program.cs
+++ DynamixelServo.TestConsole/Program.cs
@@ -8,6 +8,7 @@ using System.Threading;
 using DynamixelServo.Driver;
 using Newtonsoft.Json;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using Timer = System.Timers.Timer;
 
 namespace DynamixelServo.TestConsole
@@ -17,32 +18,27 @@ namespace DynamixelServo.TestConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Starting");
+            using (IConnection connection = new ConnectionFactory {HostName = "localhost"}.CreateConnection())
+            using(IModel channel = connection.CreateModel())
             using (DynamixelDriver driver = new DynamixelDriver("COM4"))
             {
+                channel.ExchangeDeclare("ArmPositionUpdate", "fanout");
                 foreach (var servo in driver.Search(1, 5))
                 {
                     driver.SetTorque(servo, false);
                 }
-                System.Environment.Exit(1);
-                driver.SetComplianceSlope(1, ComplianceSlope.S128);
-                // 600 - 450
-                // 600 - 700
-                driver.MoveToBlocking(3, 600);
-                driver.MoveToBlocking(1, 450);
-                driver.MoveToBlocking(3, 700);
-                driver.MoveToBlocking(3, 600);
+                while (true)
+                {
+                    float @base = DynamixelDriver.UnitsToDegrees(driver.GetPresentPosition(1));
+                    float shoulder = DynamixelDriver.UnitsToDegrees(driver.GetPresentPosition(2));
+                    float elbow = DynamixelDriver.UnitsToDegrees(driver.GetPresentPosition(3));
+                    float wrist = DynamixelDriver.UnitsToDegrees(driver.GetPresentPosition(4));
+                    byte[] body =
+                        Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(new ArmPosition(@base, shoulder, elbow, wrist)));
+                    channel.BasicPublish("ArmPositionUpdate", "", body: body);
+                    Thread.Sleep(100);
+                }
 
-                driver.MoveToBlocking(1, 500);
-                driver.MoveToBlocking(3, 700);
-                driver.MoveToBlocking(3, 600);
-
-                driver.MoveToBlocking(1, 550);
-                driver.MoveToBlocking(3, 700);
-                driver.MoveToBlocking(3, 600);
-
-                driver.MoveToBlocking(1, 600);
-                driver.MoveToBlocking(3, 700);
-                driver.MoveToBlocking(3, 600);
             }
             Console.WriteLine("Press enter to exit");
             Console.ReadLine();
@@ -178,6 +174,49 @@ namespace DynamixelServo.TestConsole
                     Thread.Sleep(2 * 1000);
                 }
             }
+        }
+
+        private static void ReportServoPosition()
+        {
+            Console.WriteLine("Starting");
+            using (IConnection connection = new ConnectionFactory { HostName = "localhost" }.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            using (DynamixelDriver driver = new DynamixelDriver("COM4"))
+            {
+                channel.ExchangeDeclare("ArmPositionUpdate", "fanout");
+                foreach (var servo in driver.Search(1, 5))
+                {
+                    driver.SetTorque(servo, false);
+                }
+                while (true)
+                {
+                    float @base = DynamixelDriver.UnitsToDegrees(driver.GetPresentPosition(1));
+                    float shoulder = DynamixelDriver.UnitsToDegrees(driver.GetPresentPosition(2));
+                    float elbow = DynamixelDriver.UnitsToDegrees(driver.GetPresentPosition(3));
+                    float wrist = DynamixelDriver.UnitsToDegrees(driver.GetPresentPosition(4));
+                    byte[] body =
+                        Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(new ArmPosition(@base, shoulder, elbow, wrist)));
+                    channel.BasicPublish("ArmPositionUpdate", "", body: body);
+                    Thread.Sleep(100);
+                }
+
+            }
+        }
+    }
+
+    class ArmPosition
+    {
+        public float Base { get; set; }
+        public float Shoulder { get; set; }
+        public float Elbow { get; set; }
+        public float Wrist { get; set; }
+
+        public ArmPosition(float @base, float shoulder, float elbow, float wrist)
+        {
+            Base = @base;
+            Shoulder = shoulder;
+            Elbow = elbow;
+            Wrist = wrist;
         }
     }
 }
