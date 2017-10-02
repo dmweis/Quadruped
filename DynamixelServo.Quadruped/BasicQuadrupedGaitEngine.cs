879a25ec38bdd5395bb7f01b1456d81159b60cfc
David Weis <davidmichaelweis@gmail.com>
2017-10-02 23:35:45 +0200

    Add simple walk in gait engine

--- DynamixelServo.Quadruped/BasicQuadrupedGaitEngine.cs
+++ DynamixelServo.Quadruped/BasicQuadrupedGaitEngine.cs
@@ -7,7 +7,7 @@ namespace DynamixelServo.Quadruped
 {
     public class BasicQuadrupedGaitEngine : QuadrupedGaitEngine
     {
-        private const int Speed = 6;
+        private const int Speed = 22;
         private float NextStepLength => Speed * 0.001f * TimeSincelastTick;
 
         private readonly Queue<MotorPositions> _moves = new Queue<MotorPositions>();
@@ -17,14 +17,27 @@ namespace DynamixelServo.Quadruped
         private const float LegHeight = -11f;
         private const int LegDistance = 15;
 
+        private MotorPositions RelaxedStance
+        {
+            get
+            {
+                return new MotorPositions
+                {
+                    LeftFront = new Vector3(-LegDistance, LegDistance, LegHeight),
+                    RightFront = new Vector3(LegDistance, LegDistance, LegHeight),
+                    LeftRear = new Vector3(-LegDistance, -LegDistance, LegHeight),
+                    RightRear = new Vector3(LegDistance, -LegDistance, LegHeight)
+                }.Copy();
+            }
+        }
+
         private readonly MotorPositions _lastWrittenPosition;
 
         public BasicQuadrupedGaitEngine(QuadrupedIkDriver driver) : base(driver)
         {
-            EnqueuePositions();
             Driver.Setup();
-            Driver.StandUpfromGround();
             _lastWrittenPosition = new MotorPositions(driver);
+            EnqueuePositions();
             _nextMove = _moves.Dequeue();
             StartEngine();
         }
@@ -44,111 +57,88 @@ namespace DynamixelServo.Quadruped
 
         public void EnqueuePositions()
         {
-            _moves.Enqueue(new MotorPositions
+            float average = (_lastWrittenPosition.LeftFront.Z +
+                             _lastWrittenPosition.RightFront.Z +
+                             _lastWrittenPosition.LeftRear.Z +
+                             _lastWrittenPosition.RightRear.Z) / 4;
+            if (average > -9)
             {
-                LeftFront = new Vector3(-LegDistance, LegDistance + 5, LegHeight),
-                RightFront = new Vector3(LegDistance, LegDistance, LegHeight),
-                LeftRear = new Vector3(-LegDistance + 2.5f, -LegDistance + 2.5f, LegHeight),
-                RightRear = new Vector3(LegDistance, -LegDistance + 5, LegHeight)
-            });
-            _moves.Enqueue(new MotorPositions
-            {
-                LeftFront = new Vector3(-LegDistance, LegDistance + 5, LegHeight),
-                RightFront = new Vector3(LegDistance, LegDistance, LegHeight + 3),
-                LeftRear = new Vector3(-LegDistance + 2.5f, -LegDistance + 2.5f, LegHeight),
-                RightRear = new Vector3(LegDistance, -LegDistance + 5, LegHeight)
-            });
-            _moves.Enqueue(new MotorPositions
-            {
-                LeftFront = new Vector3(-LegDistance, LegDistance + 5, LegHeight),
-                RightFront = new Vector3(LegDistance + 5, LegDistance + 5, LegHeight + 3),
-                LeftRear = new Vector3(-LegDistance + 2.5f, -LegDistance + 2.5f, LegHeight),
-                RightRear = new Vector3(LegDistance, -LegDistance + 5, LegHeight)
-            });
-            _moves.Enqueue(new MotorPositions
-            {
-                LeftFront = new Vector3(-LegDistance, LegDistance + 5, LegHeight),
-                RightFront = new Vector3(LegDistance + 5, LegDistance + 5, LegHeight),
-                LeftRear = new Vector3(-LegDistance + 2.5f, -LegDistance + 2.5f, LegHeight),
-                RightRear = new Vector3(LegDistance, -LegDistance + 5, LegHeight)
-            });
-            _moves.Enqueue(new MotorPositions
-            {
-                LeftFront = new Vector3(-LegDistance, LegDistance + 5, LegHeight),
-                RightFront = new Vector3(LegDistance + 5, LegDistance + 5, LegHeight + 3),
-                LeftRear = new Vector3(-LegDistance + 2.5f, -LegDistance + 2.5f, LegHeight),
-                RightRear = new Vector3(LegDistance, -LegDistance + 5, LegHeight)
-            });
-            _moves.Enqueue(new MotorPositions
-            {
-                LeftFront = new Vector3(-LegDistance, LegDistance + 5, LegHeight),
-                RightFront = new Vector3(LegDistance, LegDistance, LegHeight + 3),
-                LeftRear = new Vector3(-LegDistance + 2.5f, -LegDistance + 2.5f, LegHeight),
-                RightRear = new Vector3(LegDistance, -LegDistance + 5, LegHeight)
-            });
-            _moves.Enqueue(new MotorPositions
-            {
-                LeftFront = new Vector3(-LegDistance, LegDistance + 5, LegHeight),
-                RightFront = new Vector3(LegDistance, LegDistance, LegHeight),
-                LeftRear = new Vector3(-LegDistance + 2.5f, -LegDistance + 2.5f, LegHeight),
-                RightRear = new Vector3(LegDistance, -LegDistance + 5, LegHeight)
-            });
-        }
-    }
+                _moves.Enqueue(new MotorPositions
+                {
+                    LeftFront = new Vector3(-LegDistance, LegDistance, 0),
+                    RightFront = new Vector3(LegDistance, LegDistance, 0),
+                    LeftRear = new Vector3(-LegDistance, -LegDistance, 0),
+                    RightRear = new Vector3(LegDistance, -LegDistance, 0)
+                });
+            }
+            
+            _moves.Enqueue(RelaxedStance);
 
-    class MotorPositions
-    {
-        public Vector3 LeftFront { get; set; }
-        public Vector3 RightFront { get; set; }
-        public Vector3 LeftRear { get; set; }
-        public Vector3 RightRear { get; set; }
+            var nextStep = RelaxedStance;
+            nextStep.Transform(new Vector3(3, 0, 0));
+            _moves.Enqueue(nextStep);
 
-        public MotorPositions(QuadrupedIkDriver driver)
-        {
-            LeftFront = driver.GetLeftFrontLegGoal();
-            RightFront = driver.GetRightFrontLegGoal();
-            LeftRear = driver.GetLeftRearLegGoal();
-            RightRear = driver.GetRightRearLegGoal();
-        }
+            nextStep = nextStep.Copy();
+            nextStep.Transform(new Vector3(0, 0, 3), LegFlags.RightFront);
+            _moves.Enqueue(nextStep);
 
-        public MotorPositions(Vector3 leftFront, Vector3 rightFront, Vector3 leftRear, Vector3 rightRear)
-        {
-            LeftFront = leftFront;
-            RightFront = rightFront;
-            LeftRear = leftRear;
-            RightRear = rightRear;
-        }
+            nextStep = nextStep.Copy();
+            nextStep.Transform(new Vector3(0, 8, 0), LegFlags.RightFront);
+            _moves.Enqueue(nextStep);
 
-        public MotorPositions()
-        {
-            LeftFront = new Vector3();
-            RightFront = new Vector3();
-            LeftRear = new Vector3();
-            RightRear = new Vector3();
-        }
+            nextStep = nextStep.Copy();
+            nextStep.Transform(new Vector3(0, 0, -3), LegFlags.RightFront);
+            _moves.Enqueue(nextStep);
 
-        public void MoveTowards(MotorPositions target, float distance)
-        {
-            LeftFront = LeftFront.MoveTowards(target.LeftFront, distance);
-            RightFront = RightFront.MoveTowards(target.RightFront, distance);
-            LeftRear = LeftRear.MoveTowards(target.LeftRear, distance);
-            RightRear = RightRear.MoveTowards(target.RightRear, distance);
-        }
+            nextStep = nextStep.Copy();
+            nextStep.Transform(new Vector3(-6, -3, 0));
+            _moves.Enqueue(nextStep);
 
-        public void MoveRobot(QuadrupedIkDriver driver)
-        {
-            driver.MoveLeftFrontLeg(LeftFront);
-            driver.MoveRightFrontLeg(RightFront);
-            driver.MoveLeftRearLeg(LeftRear);
-            driver.MoveRightRearLeg(RightRear);
-        }
+            nextStep = nextStep.Copy();
+            nextStep.Transform(new Vector3(0, 0, 3), LegFlags.LeftFront);
+            _moves.Enqueue(nextStep);
 
-        public bool MoveFinished(MotorPositions other)
-        {
-            return LeftFront == other.LeftFront &&
-                   RightFront == other.RightFront &&
-                   LeftRear == other.LeftRear &&
-                   RightRear == other.RightRear;
+            nextStep = nextStep.Copy();
+            nextStep.Transform(new Vector3(0, 8, 0), LegFlags.LeftFront);
+            _moves.Enqueue(nextStep);
+
+            nextStep = nextStep.Copy();
+            nextStep.Transform(new Vector3(0, 0, -3), LegFlags.LeftFront);
+            _moves.Enqueue(nextStep);
+
+            nextStep = nextStep.Copy();
+            nextStep.Transform(new Vector3(6, -3, 0));
+            _moves.Enqueue(nextStep);
+
+            nextStep = nextStep.Copy();
+            nextStep.Transform(new Vector3(0, 0, 3), LegFlags.RightRear);
+            _moves.Enqueue(nextStep);
+
+            nextStep = nextStep.Copy();
+            nextStep.Transform(new Vector3(0, 8, 0), LegFlags.RightRear);
+            _moves.Enqueue(nextStep);
+
+            nextStep = nextStep.Copy();
+            nextStep.Transform(new Vector3(0, 0, -3), LegFlags.RightRear);
+            _moves.Enqueue(nextStep);
+
+            nextStep = nextStep.Copy();
+            nextStep.Transform(new Vector3(-6, 0, 0));
+            _moves.Enqueue(nextStep);
+
+            nextStep = nextStep.Copy();
+            nextStep.Transform(new Vector3(0, 0, 3), LegFlags.LeftRear);
+            _moves.Enqueue(nextStep);
+
+            nextStep = nextStep.Copy();
+            nextStep.Transform(new Vector3(0, 8, 0), LegFlags.LeftRear);
+            _moves.Enqueue(nextStep);
+
+            nextStep = nextStep.Copy();
+            nextStep.Transform(new Vector3(0, 0, -3), LegFlags.LeftRear);
+            _moves.Enqueue(nextStep);
+
+            _moves.Enqueue(RelaxedStance);
         }
     }
 }
---
+++ DynamixelServo.Quadruped/MotorPositions.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Numerics;
+
+namespace DynamixelServo.Quadruped
+{
+    class MotorPositions
+    {
+        public Vector3 LeftFront { get; set; }
+        public Vector3 RightFront { get; set; }
+        public Vector3 LeftRear { get; set; }
+        public Vector3 RightRear { get; set; }
+
+        public MotorPositions(QuadrupedIkDriver driver)
+        {
+            LeftFront = driver.GetLeftFrontLegGoal();
+            RightFront = driver.GetRightFrontLegGoal();
+            LeftRear = driver.GetLeftRearLegGoal();
+            RightRear = driver.GetRightRearLegGoal();
+        }
+
+        public MotorPositions(MotorPositions motorPositions)
+        {
+            LeftFront = motorPositions.LeftFront;
+            RightFront = motorPositions.RightFront;
+            LeftRear = motorPositions.LeftRear;
+            RightRear = motorPositions.RightRear;
+        }
+
+        public MotorPositions(Vector3 leftFront, Vector3 rightFront, Vector3 leftRear, Vector3 rightRear)
+        {
+            LeftFront = leftFront;
+            RightFront = rightFront;
+            LeftRear = leftRear;
+            RightRear = rightRear;
+        }
+
+        public MotorPositions()
+        {
+            LeftFront = new Vector3();
+            RightFront = new Vector3();
+            LeftRear = new Vector3();
+            RightRear = new Vector3();
+        }
+
+        public void Transform(Vector3 transformVector, LegFlags legs = LegFlags.All)
+        {
+            if ((legs & LegFlags.LeftFront) != 0)
+            {
+                LeftFront += transformVector;
+            }
+            if ((legs & LegFlags.RightFront) != 0)
+            {
+                RightFront += transformVector;
+            }
+            if ((legs & LegFlags.LeftRear) != 0)
+            {
+                LeftRear += transformVector;
+            }
+            if ((legs & LegFlags.RightRear) != 0)
+            {
+                RightRear += transformVector;
+            }
+        }
+
+        public void SetLegs(Vector3 newVector, LegFlags legs = LegFlags.All)
+        {
+            if ((legs & LegFlags.LeftFront) != 0)
+            {
+                LeftFront = newVector;
+            }
+            if ((legs & LegFlags.RightFront) != 0)
+            {
+                RightFront = newVector;
+            }
+            if ((legs & LegFlags.LeftRear) != 0)
+            {
+                LeftRear = newVector;
+            }
+            if ((legs & LegFlags.RightRear) != 0)
+            {
+                RightRear = newVector;
+            }
+        }
+
+        public void MoveTowards(MotorPositions target, float distance)
+        {
+            LeftFront = LeftFront.MoveTowards(target.LeftFront, distance);
+            RightFront = RightFront.MoveTowards(target.RightFront, distance);
+            LeftRear = LeftRear.MoveTowards(target.LeftRear, distance);
+            RightRear = RightRear.MoveTowards(target.RightRear, distance);
+        }
+
+        public void MoveRobot(QuadrupedIkDriver driver)
+        {
+            driver.MoveLeftFrontLeg(LeftFront);
+            driver.MoveRightFrontLeg(RightFront);
+            driver.MoveLeftRearLeg(LeftRear);
+            driver.MoveRightRearLeg(RightRear);
+        }
+
+        public bool MoveFinished(MotorPositions other)
+        {
+            return LeftFront == other.LeftFront &&
+                   RightFront == other.RightFront &&
+                   LeftRear == other.LeftRear &&
+                   RightRear == other.RightRear;
+        }
+
+        public MotorPositions Copy()
+        {
+            return new MotorPositions(this);
+        }
+    }
+
+    [Flags]
+    enum LegFlags
+    {
+        LeftFront = 1,
+        RightFront = 2,
+        LeftRear = 4,
+        RightRear = 8,
+        All = LeftFront | RightFront | LeftRear | RightRear,
+        Front = LeftFront | RightFront,
+        Rear = LeftRear | RightRear,
+        Right = RightFront | RightRear,
+        Left = LeftFront | LeftRear
+    }
+}
