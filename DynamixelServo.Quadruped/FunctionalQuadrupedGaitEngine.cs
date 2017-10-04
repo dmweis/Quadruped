1dc86d9384e2a14343ade7188ce97aebeaa4b295
David Weis <davidmichaelweis@gmail.com>
2017-10-04 23:46:41 +0200

    Add sinusoid driven gait generator

--- DynamixelServo.Quadruped/FunctionalQuadrupedGaitEngine.cs
+++ DynamixelServo.Quadruped/FunctionalQuadrupedGaitEngine.cs
@@ -11,6 +11,16 @@ namespace DynamixelServo.Quadruped
         private const float LegHeight = -11f;
         private const int LegDistance = 15;
 
+        /// <summary>
+        /// Forward speed
+        /// </summary>
+        private const int Speed = 9;
+        private float NextStepLength => Speed * 0.001f * TimeSincelastTick;
+
+        private bool _wasPositive = true;
+        private readonly LegPicker _legSelector = new LegPicker();
+        private MotorPositions _lastWrittenPosition;
+
         private MotorPositions RelaxedStance
         {
             get
@@ -36,34 +46,88 @@ namespace DynamixelServo.Quadruped
                              currentPosition.RightRear.Z) / 4;
             if (average > -9)
             {
-                new MotorPositions
+                Driver.MoveLegs(new MotorPositions
                 {
-                    LeftFront = new Vector3(-LegDistance + 6, LegDistance + 6, 0),
+                    LeftFront = new Vector3(-LegDistance, LegDistance, 0),
                     RightFront = new Vector3(LegDistance, LegDistance, 0),
                     LeftRear = new Vector3(-LegDistance, -LegDistance, 0),
-                    RightRear = new Vector3(LegDistance + 6, -LegDistance + 6, 0)
-                }.MoveRobot(Driver);
+                    RightRear = new Vector3(LegDistance, -LegDistance, 0)
+                });
                 Thread.Sleep(1000);
             }
-            new MotorPositions
-            {
-                LeftFront = new Vector3(-LegDistance + 6, LegDistance + 6, LegHeight),
-                RightFront = new Vector3(LegDistance, LegDistance, LegHeight),
-                LeftRear = new Vector3(-LegDistance, -LegDistance, LegHeight),
-                RightRear = new Vector3(LegDistance + 6, -LegDistance + 6, LegHeight)
-            }.MoveRobot(Driver);
+            _lastWrittenPosition = RelaxedStance;
+            Driver.MoveLegs(_lastWrittenPosition);
             StartEngine();
         }
 
+        private float _rightFrontOffset;
+        private float _rightRearOffset;
+        private float _leftFrontOffset;
+        private float _leftRearOffset;
+
         protected override void EngineSpin()
         {
-            double angle = TimeSinceStart / 1000.0 * 90.0;
-            double sin = Math.Sin(angle * Math.PI / 180);
-            double cos = Math.Cos(angle * Math.PI / 180);
+            double angle = TimeSinceStart / 1000.0 * 320.0; // this determins the speed of switching legs
+            double legLiftSin = Math.Sin(angle.DegreeToRad());
+            double swaySin = Math.Sin((angle / 2).DegreeToRad());
+            if (_wasPositive && legLiftSin < 0)
+            {
+                _wasPositive = false;
+                _legSelector.SwitchLeg();
+                Console.WriteLine($"Switched to {_legSelector.CurrentLeg}");
+            }
+            else if (!_wasPositive && legLiftSin > 0)
+            {
+                _wasPositive = true;
+                _legSelector.SwitchLeg();
+                Console.WriteLine($"Switched to {_legSelector.CurrentLeg}");
+            }
+            // shift leg offsets
+            // TODO refactor
+            if (_legSelector.CurrentLeg == LegFlags.RightFront)
+            {
+                _rightFrontOffset += 3 * NextStepLength;
+            }
+            else
+            {
+                _rightFrontOffset -= NextStepLength;
+            }
+            if (_legSelector.CurrentLeg == LegFlags.RightRear)
+            {
+                _rightRearOffset += 3 * NextStepLength;
+            }
+            else
+            {
+                _rightRearOffset -= NextStepLength;
+            }
+            if (_legSelector.CurrentLeg == LegFlags.LeftFront)
+            {
+                _leftFrontOffset += 3 * NextStepLength;
+            }
+            else
+            {
+                _leftFrontOffset -= NextStepLength;
+            }
+            if (_legSelector.CurrentLeg == LegFlags.LeftRear)
+            {
+                _leftRearOffset += 3 * NextStepLength;
+            }
+            else
+            {
+                _leftRearOffset -= NextStepLength;
+            }
+
+            var newPosition = RelaxedStance;
+            newPosition.Transform(new Vector3((float)(swaySin * 4), 0, 0));
+            newPosition.Transform(new Vector3(0, _rightFrontOffset, 0), LegFlags.RightFront);
+            newPosition.Transform(new Vector3(0, _rightRearOffset, 0), LegFlags.RightRear);
+            newPosition.Transform(new Vector3(0, _leftFrontOffset, 0), LegFlags.LeftFront);
+            newPosition.Transform(new Vector3(0, _leftRearOffset, 0), LegFlags.LeftRear);
+            newPosition.Transform(new Vector3(0, 0, (float)Math.Abs(legLiftSin) * 3), _legSelector.CurrentLeg);
+            _lastWrittenPosition = newPosition;
             try
             {
-                //Driver.MoveAbsoluteCenterMass(new Vector3((float)(6 * sin), -(float)(6 * cos), 0), LegDistance, LegHeight );
-                Driver.MoveRightFrontLeg(new Vector3((float)(cos * 4.0) + LegDistance, -(float)(cos * 4.0) + LegDistance, (float)Math.Abs(sin * 4) + LegHeight));
+                Driver.MoveLegs(_lastWrittenPosition);
             }
             catch (IOException e)
             {
@@ -73,5 +137,15 @@ namespace DynamixelServo.Quadruped
                 throw;
             }
         }
+    }
+
+    class LegPicker
+    {
+        public LegFlags CurrentLeg => _legs[_currentLegIndex];
+
+        private readonly LegFlags[] _legs = { LegFlags.RightRear, LegFlags.RightFront, LegFlags.LeftRear, LegFlags.LeftFront };
+        private int _currentLegIndex;
+
+        public void SwitchLeg() => _currentLegIndex = _currentLegIndex == _legs.Length - 1 ? 0 : _currentLegIndex + 1;
     }
 }
