538638c38d6a0fd6e7df56c6df417fe2b8cafd11
David Weis <davidmichaelweis@gmail.com>
2017-10-04 18:57:33 +0200

    Move leg flags to a separate file

--- DynamixelServo.Quadruped/MotorPositions.cs
+++ DynamixelServo.Quadruped/MotorPositions.cs
@@ -111,18 +111,4 @@ namespace DynamixelServo.Quadruped
             return new MotorPositions(this);
         }
     }
-
-    [Flags]
-    enum LegFlags
-    {
-        LeftFront = 1,
-        RightFront = 2,
-        LeftRear = 4,
-        RightRear = 8,
-        All = LeftFront | RightFront | LeftRear | RightRear,
-        Front = LeftFront | RightFront,
-        Rear = LeftRear | RightRear,
-        Right = RightFront | RightRear,
-        Left = LeftFront | LeftRear
-    }
 }
