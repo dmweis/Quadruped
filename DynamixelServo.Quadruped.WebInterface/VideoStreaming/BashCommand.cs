43384861d845f86c008d102799b8d4311a1f4fd0
David Weis <davidmichaelweis@gmail.com>
2017-10-27 07:28:17 +0200

    Add video streamer using bash invokation

---
+++ DynamixelServo.Quadruped.WebInterface/VideoStreaming/BashCommand.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+
+namespace DynamixelServo.Quadruped.WebInterface.VideoStreaming
+{
+    public class BashCommand
+    {
+        private const char CommandSeparator = '\n';
+
+        private readonly string _command;
+
+        private BashCommand(string command)
+        {
+            _command = command;
+        }
+
+        public static BashCommand Command(string command)
+        {
+            return new BashCommand(command);
+        }
+
+        public BashCommand AddCommand(string command)
+        {
+            return new BashCommand(_command + CommandSeparator + command);
+        }
+
+        public string Execute()
+        {
+            var excaped = Escape(_command);
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"{excaped}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+            process.Start();
+            var result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return result;
+        }
+
+        private string Escape(string command)
+        {
+            return command.Replace("\"", "\\\"");
+        }
+    }
+}
---
+++ DynamixelServo.Quadruped.WebInterface/VideoStreaming/VideoStreamingService.cs
@@ -0,0 +1,35 @@
+﻿namespace DynamixelServo.Quadruped.WebInterface.VideoStreaming
+{
+    public class VideoStreamingService
+    {
+
+        public string StreamPath { get; set; }
+
+        public VideoStreamingService()
+        {
+            var result = BashCommand
+                .Command("ls")
+                .AddCommand("mkdir TestFolder")
+                .AddCommand("cd TestFolder")
+                .AddCommand("echo 'Test test test ' > test.txt")
+                .Execute();
+        }
+
+        public void StartStream(int port)
+        {
+            BashCommand
+                .Command($"mjpg_streamer -o \"output_http.so -p {port}\" -i \"input_raspicam.so -x 1280 -y 720 -fps 10\"")
+                .Execute();
+            var ip = BashCommand.Command("hostname -i").Execute();
+            StreamPath = $"http://{ip}:{port}/?action=stream";
+        }
+
+        public void KillStream()
+        {
+            BashCommand
+                .Command("pkill mjpg_streamer")
+                .Execute();
+            StreamPath = string.Empty;
+        }
+    }
+}
