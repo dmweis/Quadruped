dfa2aed6e9ee0e01d79cd9ff5389b6fa5e2093b1
David Weis <davidmichaelweis@gmail.com>
2017-10-28 18:49:47 +0200

    Start video streaming process directly

--- DynamixelServo.Quadruped.WebInterface/VideoStreaming/VideoStreamingService.cs
+++ DynamixelServo.Quadruped.WebInterface/VideoStreaming/VideoStreamingService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace DynamixelServo.Quadruped.WebInterface.VideoStreaming
@@ -6,24 +7,32 @@ namespace DynamixelServo.Quadruped.WebInterface.VideoStreaming
     public class VideoStreamingService : IVideoService
     {
         public bool StreamRunning { get; private set; }
+        private readonly Process _streamerProcess;
 
         public VideoStreamingService(IApplicationLifetime applicationLifetime, ILogger<VideoStreamingService> logger)
         {
             applicationLifetime.ApplicationStopping.Register(KillStream);
             int port = 8080;
             logger.LogInformation("Starting video server");
-            BashCommand
-                .Command($"nohup mjpg_streamer -o \"output_http.so -p {port}\" -i \"input_raspicam.so -x 1280 -y 720 -fps 10\" &")
-                .Execute();
+            _streamerProcess = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/usr/local/bin/mjpg_streamer",
+                    Arguments = $" -o \"output_http.so -p {port}\" -i \"input_raspicam.so -x 1280 -y 720 -fps 10",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+            _streamerProcess.Start();
             StreamRunning = true;
             logger.LogInformation("Video server up and running");
         }
 
         public void KillStream()
         {
-            BashCommand
-                .Command("pkill mjpg_streamer")
-                .Execute();
+            _streamerProcess.Close();
             StreamRunning = false;
         }
     }
