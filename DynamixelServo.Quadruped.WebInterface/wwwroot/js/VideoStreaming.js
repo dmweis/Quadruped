4d47a904bf9141c375b20cd4d4e8c501d99129ff
David Weis <davidmichaelweis@gmail.com>
2017-10-28 16:58:40 +0200

    Fix video stream trying to use the wrong protocol

--- DynamixelServo.Quadruped.WebInterface/wwwroot/js/VideoStreaming.js
+++ DynamixelServo.Quadruped.WebInterface/wwwroot/js/VideoStreaming.js
@@ -1,4 +1,4 @@
 ﻿window.onload = function () {
     let videoImage = document.getElementById('video_image');
-    videoImage.src = `ws://${document.domain}:8080/?action=stream`;
+    videoImage.src = `http://${document.domain}:8080/?action=stream`;
 };
