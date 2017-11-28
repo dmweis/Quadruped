a4285203af6b67368d15cf0a2a9dc9c0eaef19ee
David Weis <davidmichaelweis@gmail.com>
2017-11-28 06:37:00 +0100

    Switch to using a watcher that subscribes on two props

--- Quadruped.WebInterface/wwwroot/js/site.js
+++ Quadruped.WebInterface/wwwroot/js/site.js
@@ -1,5 +1,5 @@
 ﻿function createJoystick(data) {
-    return new Vue({
+    const vm = new Vue({
         el: data.elementId,
         data: {
             angle: 0,
@@ -9,44 +9,39 @@
         },
         mounted: function () {
             const context = this;
-            nipplejs.create({ zone: this.$el, color: this.color }).on('added', function (evt, nipple) {
-                nipple.on('move',
-                    function (evt, arg) {
-                        context.angle = arg.angle.radian;
-                        context.distance = arg.distance;
-                    });
-                nipple.on('start',
-                    function () {
-                        if (data.startCallback) {
-                            data.startCallback();
-                        }
-                        context.angle = 0;
-                        context.distance = 0;
-                    });
-                nipple.on('end',
-                    function () {
-                        context.angle = 0;
-                        context.distance = 0;
-                    });
-            });
-        },
-        watch: {
-            angle: function () {
-                data.socket.send(JSON.stringify({
-                    joystickType: this.name,
-                    angle: this.angle,
-                    MessageType: 'movement',
-                    distance: this.distance
-                }));
-            },
-            distance: function () {
-                data.socket.send(JSON.stringify({
-                    joystickType: this.name,
-                    angle: this.angle,
-                    MessageType: 'movement',
-                    distance: this.distance
-                }));
-            }
+            nipplejs.create({ zone: this.$el, color: this.color }).on('added',
+                function (evt, nipple) {
+                    nipple.on('move',
+                        function (evt, arg) {
+                            context.angle = arg.angle.radian;
+                            context.distance = arg.distance;
+                        });
+                    nipple.on('start',
+                        function () {
+                            if (data.startCallback) {
+                                data.startCallback();
+                            }
+                            context.angle = 0;
+                            context.distance = 0;
+                        });
+                    nipple.on('end',
+                        function () {
+                            context.angle = 0;
+                            context.distance = 0;
+                        });
+                });
         }
     });
+    vm.$watch(function () {
+        return this.angle + this.distance;
+    },
+        function (newValue, oldValue) {
+            data.socket.send(JSON.stringify({
+                joystickType: this.name,
+                angle: this.angle,
+                MessageType: 'movement',
+                distance: this.distance
+            }));
+        });
+    return vm;
 }
