328cb8027ca4f748abfadec2051ffa15738211b0
David Weis <davidmichaelweis@gmail.com>
2018-01-07 09:46:10 +0100

    Add test console

---
+++ Quadruped.TestConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Quadruped.Driver;
+
+namespace Quadruped.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting");
+            using (var driver = new DynamixelDriver("COM4"))
+            using (var ikDriver = new QuadrupedIkDriver(driver))
+            using (var controller = new SmartQuadrupedController(gaiteEngine))
+            {
+                controller.
+                Console.WriteLine("Done!");
+                Console.ReadLine();
+            }
+            Console.WriteLine("Press enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
